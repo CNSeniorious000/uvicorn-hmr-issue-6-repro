c5e4bf9d1f7c007bd7760c813eaf283cc2cd007d
Muspi Merol <me@promplate.dev>
2025-04-22 22:16:19 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Reproduction
+
+https://github.com/promplate/hmr/issues/6
+
+```sh
+uv sync
+python -m app
+```
---
+++ app/__main__.py
@@ -0,0 +1,20 @@
+# __main__.py
+from pathlib import Path
+import sys
+import uvicorn_hmr
+
+if __name__ == "__main__":
+    app_path = 'src.server:app'  
+    cwd = Path.cwd()
+    
+    if str(cwd) not in sys.path:
+        sys.path.insert(0, str(cwd))
+
+    uvicorn_hmr.main(
+        slug=app_path,
+        host="0.0.0.0",
+        port=8000,
+        log_level="debug",
+        reload_include=str(cwd / "src"),
+        reload=False
+    )
