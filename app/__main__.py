3d5e3e1bd0208cb43a77ee1b619acb1851f36002
Muspi Merol <me@promplate.dev>
2025-04-22 22:24:37 +0800

    found the problem

--- app/__main__.py
+++ app/__main__.py
@@ -1,12 +1,17 @@
+# import src.server
+# uncomment the above line will cause issue
+# AttributeError: module 'src.server' has no attribute 'load'
+
+
 # __main__.py
 from pathlib import Path
 import sys
 import uvicorn_hmr
 
 if __name__ == "__main__":
-    app_path = 'src.server:app'  
+    app_path = "src.server:app"
     cwd = Path.cwd()
-    
+
     if str(cwd) not in sys.path:
         sys.path.insert(0, str(cwd))
 
@@ -16,5 +21,5 @@ if __name__ == "__main__":
         port=8000,
         log_level="debug",
         reload_include=str(cwd / "src"),
-        reload=False
+        reload=False,
     )
